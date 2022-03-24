eef05ea7add6977b59f010f50329b7375a58fd9b
Nathan Connacher <nconnacher89@southhills.edu>
2022-03-24 09:01:27 -0400

    Moved jumbotron container for weather to component "currentWeatherContainer.vue" Created test API server in python on Repl.it: https://weather-station-api.draketamer.repl.co/

---
+++ src/components/icons/currentWeatherContainer.vue
@@ -0,0 +1,55 @@
+<script>
+export default {
+    data() {
+        return {
+            realTemp: 0,
+            feelsLike: 0,
+            humidity: 0,
+            windSpeed: 0,
+        }
+    },
+    async mounted() {
+
+        //Async-Await Based Way
+        const response = await fetch("https://weather-station-api.draketamer.repl.co/")
+        const data = await response.json()
+        this.realTemp = data.weatherData.realTemp
+        this.feelsLike = data.weatherData.feelsLike
+        this.windSpeed = data.weatherData.windSpeed
+        this.humidity = data.weatherData.humidity
+    }
+}
+</script>
+
+<template>
+    <div class="jumbotron">
+      <h1>South Hills Altoona</h1>
+      <div class="row marketing">
+        <div class="col-lg-6">
+          <h4>Temp</h4>
+          <p>{{ realTemp }}&deg;F</p>
+        </div>
+
+        <div class="col-lg-6">
+          <h4>Feels Like</h4>
+          <p>{{ feelsLike }}&deg;F</p>
+        </div>
+      </div>
+
+      <div class="row marketing">
+        <div class="col-lg-6">
+          <h4>Humidity</h4>
+          <p>{{ humidity }}%</p>
+        </div>
+
+        <div class="col-lg-6">
+          <h4>Wind Speed</h4>
+          <p>{{ windSpeed }}mph</p>
+        </div>
+      </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,41 @@
 <script setup>
+import currentWeatherContainer from "../components/icons/currentWeatherContainer.vue";
 
-</script>
+
+
+  components: { currentWeatherContainer }</script>
 
 <template>
-  
+  <div class="container">
+    <div class="header clearfix">
+      <nav>
+        <ul class="nav nav-pills pull-right">
+          <li role="presentation" class="active">
+            <a href="#">Home</a>
+          </li>
+        </ul>
+      </nav>
+      <h3 class="text-muted">South Hills Weather Station</h3>
+    </div>
+
+    <current-weather-container />
+
+    <div class="row marketing">
+      <div class="card">
+        <h4>Subheading</h4>
+        <img src="/southhillsBackground.jpg" class="card-img-top" />
+        <div class="card-body">
+          <h5 class="card-title">Card Title</h5>
+          <p
+            class="card-text"
+          >Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+        </div>
+      </div>
+    </div>
+
+    <footer class="footer">
+      <p>&copy; 2022 Ender Malcolm, South Hills</p>
+    </footer>
+  </div>
+  <!-- /container -->
 </template>
